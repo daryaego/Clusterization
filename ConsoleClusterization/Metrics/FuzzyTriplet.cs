19a30aee9c5f211b46cd601751c6399740bc1178
Darya Ego <dar3997@yandex.ru>
2019-05-27 01:47:37 +0300

    Created FuzzyTriplet.cs as well #4

---
+++ ConsoleClusterization/Metrics/FuzzyTriplet.cs
@@ -0,0 +1,20 @@
+﻿using Clusters.Metrica;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleClusterization.Metrics
+{
+    public class FuzzyTriplet : FuzzyObject
+    {
+        public Triplet<FuzzyObject> triplet;
+
+        public FuzzyTriplet()
+        { }
+
+        public FuzzyTriplet(FuzzyObject f, FuzzyObject s, FuzzyObject t)
+        {
+            triplet = new Triplet<FuzzyObject>(f, s, t);
+        }
+    }
+}
