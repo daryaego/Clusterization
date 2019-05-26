1bcbc7f885fb8a00884226d1ae3f194eba69f612
Darya Ego <dar3997@yandex.ru>
2019-05-27 01:45:35 +0300

    Created FuzzyObject.cs #4

---
+++ ConsoleClusterization/Metrics/FuzzyObject.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleClusterization.Metrics
+{
+    public class FuzzyObject
+    {
+        public Dictionary<string, Dictionary<string, double>> Belongings;
+
+        public FuzzyObject()
+        { }
+
+        public static FuzzyObject operator +(FuzzyObject first, FuzzyObject second)
+        {
+            var result = new FuzzyObject();
+            foreach (var property in first.Belongings)
+            {
+                result.Belongings.Add(property.Key, new Dictionary<string, double>());
+                if (second.Belongings.ContainsKey(property.Key))
+                {
+                    foreach (var value in property.Value)
+                        if (second.Belongings[property.Key].ContainsKey(value.Key))
+                            result.Belongings[property.Key].Add(value.Key, value.Value + second.Belongings[property.Key][value.Key]);
+                        else
+                            result.Belongings[property.Key].Add(value.Key, value.Value);
+                }
+            }
+
+            foreach (var property in second.Belongings)
+            {
+                if (!result.Belongings.ContainsKey(property.Key))
+                    result.Belongings.Add(property.Key, new Dictionary<string, double>());
+                foreach (var value in second.Belongings[property.Key])
+                    if (!result.Belongings[property.Key].ContainsKey(value.Key))
+                        result.Belongings[property.Key].Add(value.Key, value.Value);
+            }
+
+            return result;
+        }
+
+
+    }
+}
